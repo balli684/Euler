14003a611aa84f9141be6a2c42df5f99e2ee2129
Niels Ballis <niels@ballis.nl>
2023-11-28 00:02:51 +0100

    playing around

--- Euler/Program.cs
+++ Euler/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables
+            string line = "Hello, World!";
+
+            // Write the result to the console
+            Console.WriteLine(line);
+            Console.ReadLine();
+        }
+    }
+}
+
+// Console.WriteLine("Hello, World!");
