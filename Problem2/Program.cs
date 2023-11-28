17e94c84cc8c74e24c2db09abd0e96a87e272112
Niels Ballis <niels@ballis.nl>
2023-11-29 00:03:24 +0100

    problem 2

---
+++ Problem2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            var max = 4000000;
+            var current = 0;
+            var previous = 1;
+            var next = 0;
+
+            do
+            {
+                if (current % 2 == 0)
+                {
+                    sum += current;
+                }
+                next = current + previous;
+                previous = current;
+                current = next;
+            } while (current < max);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
